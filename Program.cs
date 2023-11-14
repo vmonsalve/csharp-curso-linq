2055918c60effa6803ed82560a026ee94954aebf
Vicente  Monsalve (ACChi) <141170551+vm-acchi@users.noreply.github.com>
2023-11-14 11:53:48 -0300

    First sintaxis Method or Lambda.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] numeros = { 1, 2, 3, 4, 5 };
+
+
+/*
+    Sintaxis 1:
+    * De Metodos o Lambda.
+*/
+var numerosPares = numeros.Where(n => n % 2 == 0).ToList();
+
+
+Console.WriteLine(numerosPares);
